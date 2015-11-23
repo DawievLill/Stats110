8a237981aa19423079aaf17569cca27b24ccd067
Dawie van Lill <djvanlill@gmail.com>
2015-11-23 14:05:59 +0200

    Made some defitional contributions to the script

---
+++ Chapter1.R
@@ -0,0 +1,11 @@
+#Let us create a new script for this project, to see how things work. 
+
+#It seems that I can create scripts and git will pick them up. Can't use the nice user interface that is native to R.
+
+v <- c(3,1,4,1,5,9) #Concatenate
+
+sum(v) #Sum adds the entries of the vector
+
+max(v) #Gives the largest value, whereas, min(v) gives the smallest 
+
+length(v) #Provides the length . 
